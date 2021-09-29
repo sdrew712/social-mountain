9389c51ea56d84ded01fd99052cd32ca21bfb6d0
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-28 23:04:56 -0700

    add searchPostFn

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Search.css';
+import "./Search.css";
 
-import SearchIcon from 'react-icons/lib/md/search';
+import SearchIcon from "react-icons/lib/md/search";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: "",
+    };
+  }
+
   render() {
+    const { searchText } = this.state;
+
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input
+            placeholder="Search Your Feed"
+            value={searchText}
+            onChange={({ target }) => {
+              this.setState({ searchText: target.value });
+            }}
+          />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon
+            id="Search__icon"
+            onClick={() => {
+              this.props.searchPostFn(this.state.searchText);
+            }}
+          />
         </div>
-        
       </section>
-    )
+    );
   }
 }
